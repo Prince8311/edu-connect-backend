90d0e4b9a0edf739e228664ad62b31ad6b400807
Prince8311 <sourishmondal.vizac@gmail.com>
2026-02-19 00:52:15 +0530

    first commit

---
+++ utils/headers.php
@@ -0,0 +1,31 @@
+<?php
+ini_set('session.cookie_samesite', 'None');
+ini_set('session.cookie_secure', 'true');
+session_set_cookie_params([
+    'lifetime' => 86400,
+    'path' => '/',
+    'domain' => '.ticketbay.in',
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'None'
+]);
+session_start();
+$allowedOrigins = [
+    'http://localhost:3000'
+];
+$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+if (in_array($origin, $allowedOrigins, true)) {
+    header("Access-Control-Allow-Origin: $origin");
+}
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+header("Access-Control-Expose-Headers: X-Token-Refreshed, X-New-Token");
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if ($requestMethod == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+    exit();
+}
