a23c387a15f66130b7a6e8fca705a99702a662e5
Prince8311 <sourishmondal.vizac@gmail.com>
2026-03-17 02:35:14 +0530

    feat: students

--- admin/auth/check-auth.php
+++ admin/auth/check-auth.php
@@ -20,14 +20,28 @@ if ($requestMethod === 'GET') {
     global $conn;
     $userId = mysqli_real_escape_string($conn, $authResult['userId']);
 
-    // Fetch user
-    $sql = "SELECT * FROM `admin_users` WHERE `id` = '$userId'";
+    $sql = "SELECT a.id, a.name, a.image, a.email, a.phone, a.status, a.user_role, i.inst_id, i.location FROM admin_users a JOIN institutions i ON i.admin_id = a.id WHERE a.id = '$userId' LIMIT 1";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
         $user = mysqli_fetch_assoc($result);
+        $instId = $user['inst_id'];
 
-        // Final response
+        $sessionSql = "SELECT sesssion_name, start_date, end_date FROM academic_sessions WHERE inst_id = '$instId' AND status = 'Ongoing' LIMIT 1";
+        $sessionResult = mysqli_query($conn, $sessionSql);
+        $session = null;
+
+        if ($sessionResult && mysqli_num_rows($sessionResult) > 0) {
+            $sessionRow = mysqli_fetch_assoc($sessionResult);
+
+            $session = [
+                "name" => $sessionRow['sesssion_name'],
+                "start" => $sessionRow['start_date'],
+                "end" => $sessionRow['end_date']
+            ];
+        }
+
+        $user['session'] = $session;
         $data = [
             'status' => 200,
             'message' => 'Authenticated',
