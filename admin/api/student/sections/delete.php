5d91a9cfd01a26d1d4d2601510657566ceb1cf92
Prince8311 <sourishmondal.vizac@gmail.com>
2026-03-17 22:56:27 +0530

    feat: staff

--- admin/api/student/sections/delete.php
+++ admin/api/student/sections/delete.php
@@ -1,1 +1,89 @@
-<?php ?>
+<?php
+
+require "../../../../utils/headers.php";
+require "../../../../utils/middleware.php";
+
+$authResult = adminAuthenticateRequest();
+if (!$authResult['authenticated']) {
+    $data = [
+        'status' => $authResult['status'],
+        'message' => $authResult['message']
+    ];
+    header("HTTP/1.0 " . $authResult['status']);
+    echo json_encode($data);
+    exit;
+}
+
+if ($requestMethod === 'POST') {
+    require "../../../../_db-connect.php";
+    global $conn;
+    $userId = mysqli_real_escape_string($conn, $authResult['userId']);
+
+    $inputData = json_decode(file_get_contents("php://input"), true);
+
+    if (empty($inputData)) {
+        $data = [
+            'status' => 400,
+            'message' => 'Empty request data'
+        ];
+        header("HTTP/1.0 400 Bad Request");
+        echo json_encode($data);
+        exit;
+    }
+
+    $adminSql = "SELECT i.inst_id FROM admin_users a JOIN institutions i ON a.id = i.admin_id WHERE a.id = '$userId' LIMIT 1";
+    $adminResult = mysqli_query($conn, $adminSql);
+
+    if (!$adminResult || mysqli_num_rows($adminResult) === 0) {
+        echo json_encode([
+            "status" => 401,
+            "message" => "Invalid token or institute not found"
+        ]);
+        exit;
+    }
+
+    $adminData = mysqli_fetch_assoc($adminResult);
+    $instituteId = $adminData['inst_id'];
+
+    $sectionId = mysqli_real_escape_string($conn, $inputData['sectionId']);
+    $section = mysqli_real_escape_string($conn, $inputData['section']);
+
+    $checkSql = "SELECT * FROM `student_form_sections` WHERE `id`='$sectionId' AND `inst_id`='$instituteId' AND `form_section`='$section'";
+    $checkResult = mysqli_query($conn, $checkSql);
+
+    if ($checkResult && mysqli_num_rows($checkResult) === 0) {
+        $data = [
+            'status' => 400,
+            'message' => "This section doesn't exists."
+        ];
+        header("HTTP/1.0 400 Bad request");
+        echo json_encode($data);
+        exit;
+    }
+
+    $deleteSql = "DELETE FROM `student_form_sections` WHERE `id`='$sectionId' AND `inst_id`='$instituteId' AND `form_section`='$section'";
+    $deleteResult = mysqli_query($conn, $deleteSql);
+
+    if ($deleteResult) {
+        $data = [
+            'status' => 200,
+            'message' => "Section removed successfully."
+        ];
+        header("HTTP/1.0 200 OK");
+        echo json_encode($data);
+    } else {
+        $data = [
+            'status' => 500,
+            'message' => 'Database error: ' . mysqli_error($conn)
+        ];
+        header("HTTP/1.0 500 Internal Server Error");
+        echo json_encode($data);
+    }
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
