5dc552fa67bebac6bd87751a18cef2046229f97a
Prince8311 <sourishmondal.vizac@gmail.com>
2026-04-04 20:36:23 +0530

    feat: user create

--- admin/api/institutions/create.php
+++ admin/api/institutions/create.php
@@ -1,7 +1,7 @@
 <?php
 
 require "../../../utils/headers.php";
-require "/utils/middleware.php";
+require "../../../utils/middleware.php";
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
