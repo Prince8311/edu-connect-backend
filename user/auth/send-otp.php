aa94b5db2b634080167f2aee35aefbd0a838888b
Prince8311 <sourishmondal.vizac@gmail.com>
2026-04-07 22:58:27 +0530

    feat: profile

--- user/auth/send-otp.php
+++ user/auth/send-otp.php
@@ -54,6 +54,7 @@ if ($requestMethod === 'POST') {
                     ];
                     header("HTTP/1.1 403 Forbidden");
                     echo json_encode($response);
+                    exit;
                 }
 
                 $mail = new PHPMailer(true);
@@ -184,6 +185,7 @@ if ($requestMethod === 'POST') {
                     ];
                     header("HTTP/1.1 403 Forbidden");
                     echo json_encode($response);
+                    exit;
                 }
 
                 $key = getenv('SMS_API_KEY');
