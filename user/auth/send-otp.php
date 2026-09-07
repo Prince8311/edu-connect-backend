ac3c63214ea14723179dece966f62f87fb8b00f4
Prince8311 <sourishmondal.vizac@gmail.com>
2026-09-08 01:18:59 +0530

    feat: fee collection

--- user/auth/send-otp.php
+++ user/auth/send-otp.php
@@ -111,7 +111,7 @@ if ($requestMethod === 'POST') {
                                                 <div style="position: relative; width: 100%;">
                                                     <div style="position: relative; background: #FFF; padding: 25px; border-radius: 10px; text-align: center;">
                                                         <div class="logo" style="position: relative; text-align: center;"><img
-                                                                src="https://api.edu-connect.ticketbay.in/images/logo.png" alt="Logo" style="height: 25px;"></div>
+                                                                src="https://api.edu-connect.in/images/logo.png" alt="Logo" style="height: 25px;"></div>
                                                         <div
                                                             style="position: relative; width: 300px; padding: 20px; margin: 0 auto; margin-top: 25px; background-color: #FFF;  border-radius: 10px; box-shadow: 0 0 10px rgba(126, 126, 126, 0.3);">
                                                             <div style="position: relative; font-size: 18px; font-weight: 500;">Verify Your Identity</div>
@@ -126,7 +126,7 @@ if ($requestMethod === 'POST') {
                                                                 style="background-color: #E6E7E8; border-radius: 999px; padding: 6px 20px 6px 16px;">
                                                                 <tr>
                                                                     <td style="vertical-align: middle;">
-                                                                        <img src="https://api.edu-connect.ticketbay.in/images/security.svg" alt="Secure"
+                                                                        <img src="https://api.edu-connect.in/images/security.svg" alt="Secure"
                                                                             style="width: 12px; display: block;">
                                                                     </td>
                                                                     <td style="vertical-align: middle; padding-left: 5px; padding-bottom: 3px;">
